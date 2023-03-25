ccb53d71f934192eaa3003f59425815bd51499cf
Segimara <76587473+Segimara@users.noreply.github.com>
2023-03-25 23:26:36 +0200

    delete useless files

--- BasicOutline/BasicWebApi/BasicWebApi/Data/Interfaces/IScheduleDbContext.cs
+++ BasicOutline/BasicWebApi/BasicWebApi/Data/Interfaces/IScheduleDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace BasicWebApi.Data.Interfaces
+﻿using BasicWebApi.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicWebApi.Data.Interfaces
 {
     public interface IScheduleDbContext
     {
+        public DbSet<ImageModel> Images { get; set; }
+        public DbSet<EventModel> Events { get; set; }
+
     }
 }
