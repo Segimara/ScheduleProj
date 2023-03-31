fcf5fa29fd291cbcc7d60c7a525704f411363460
Segimara <76587473+Segimara@users.noreply.github.com>
2023-04-01 02:20:48 +0300

    fix update event dto

--- BasicOutline/BasicWebApi/BasicWebApi/Dtos/UpdateEventDto.cs
+++ BasicOutline/BasicWebApi/BasicWebApi/Dtos/UpdateEventDto.cs
@@ -1,10 +1,11 @@
 ﻿using AutoMapper;
+using BasicWebApi.Core.App.Common.Mapping;
 using BasicWebApi.Core.App.Event.Commands.Create;
 using BasicWebApi.Core.App.Event.Commands.Update;
 
 namespace BasicWebApi.Controllers.Dtos
 {
-	public class UpdateEventDto
+	public class UpdateEventDto : IMapWith<UpdateEventCommand>
 	{
 		public string Title { get; set; }
 		public string Description { get; set; }
