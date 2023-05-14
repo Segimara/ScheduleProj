529dd85298a69a8de921d56689866b8a6d859af5
Segimara <76587473+Segimara@users.noreply.github.com>
2023-05-15 01:25:59 +0300

    remove tested proj

--- BasicOutline/ScheduleUI/src/app/Utility/auth/auth-provider.ts
+++
@@ -1,7 +0,0 @@
-import { UserManager } from "oidc-client";
-
-type AuthProviderProps = {
-  userManager: UserManager;
-}
-
-const AuthProvider = ({ userManager, children }: React.PropsWithChildren<AuthProviderProps>) => { 
